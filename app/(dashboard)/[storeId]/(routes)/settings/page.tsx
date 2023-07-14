aee1101eb53ea16ca34e6afe7d6323f327b76c19
Andrii <andrii.khomik@gmail.com>
2023-07-14 17:33:28 +0300

    implement updating store name

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismaDb from "@/lib/prismaDb";
+import SettingsForm from "./components/SettingsForm";
+
+interface SttingsProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingPage: FC<SttingsProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismaDb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingPage;
